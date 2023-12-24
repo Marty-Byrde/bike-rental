15b06c6f0a8c4f1ea4eee223c118d97b35d6c78e
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-12-24 16:35:49 +0100

    refactor: fixed heading and page-function name

--- app/bikeModels/page.tsx
+++ app/bikeModels/page.tsx
@@ -2,12 +2,12 @@ import { WithId } from 'mongodb'
 import { BikeModel } from '@/typings/Bike'
 import RenderBikeModel from '@/app/(components)/bikeModel/RenderBikeModel'
 
-export default async function ManagementBikeModelsPage() {
+export default async function BikeModelsPage() {
   const bikeModels: WithId<BikeModel>[] = await fetch('http://localhost/api/bikeModels', { cache: 'no-cache', next: { tags: ['manage-bikeModels'] } }).then((res) => res.json())
 
   return (
     <>
-      <h1 className='mb-8 text-center text-3xl font-semibold lg:text-left'>Manage Bike Models</h1>
+      <h1 className='mb-8 text-center text-3xl font-semibold lg:text-left'>Available Bike Models</h1>
       <div className='grid grid-cols-1 gap-4 px-2 xl:grid-cols-2 2xl:grid-cols-2 3xl:grid-cols-3'>
         {bikeModels.map((b) => (
           <RenderBikeModel key={b._id.toString()} bikeModel={b} />

9b55dc86005e77eeb0c63240dd4e0900711513ef
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-12-24 09:06:38 +0100

    feat: created update bike server action

---
+++ actions/bikes/UpdateBikeAction.ts
@@ -0,0 +1,10 @@
+'use server'
+import { revalidateTag } from 'next/cache'
+import { ObjectId, WithId } from 'mongodb'
+import BikeModel from '@/typings/Bike'
+import { updateBike } from '@/lib/bikes/BikesDAO'
+
+export default async function UpdateBikeAction(_id: ObjectId | string, bike: WithId<BikeModel>) {
+  await updateBike(_id, bike)
+  revalidateTag('manage-bikes')
+}

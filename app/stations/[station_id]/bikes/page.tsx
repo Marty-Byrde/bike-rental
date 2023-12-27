8dc9bf496efd5bb2f59083e084f52d09c0bba5f5
Marty-Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2023-12-27 12:13:25 +0100

    refactor: updated /statopns/[station_id]/bikes page
    This page now uses the RenderParkingPlaceTickets component that shows the basic informations of bikes that are parked at the parking-places of a given station.

--- app/stations/[station_id]/bikes/page.tsx
+++ app/stations/[station_id]/bikes/page.tsx
@@ -1,6 +1,6 @@
 import { getStation } from '@/lib/station/StationDAO'
 import { notFound } from 'next/navigation'
-import RenderBikeModel from '@/app/(components)/bikeModel/RenderBikeModel'
+import RenderParkingPlaceTickets from '@/app/(components)/stations/book/RenderParkingPlaceTickets'
 
 interface SearchParams {
   params: {
@@ -25,20 +25,7 @@ export default async function StationBikesPage({ params: { station_id } }: Searc
   return (
     <div>
       <h1 className='mb-8 text-center text-3xl font-semibold lg:text-left'>Bikes of {station.name}</h1>
-      <div className='flex gap-4'>
-        {usedParkingPlaces?.map((up) => (
-          <div key={up.id} className='flex flex-col gap-2 rounded-md px-4 py-2 dark:bg-neutral-700/40'>
-            <div className='flex flex-col gap-4 2sm:flex-row'>
-              <span className='font-semibold text-gray-700 dark:text-gray-200'>Parked at:</span>
-              <span>Parking-Place-{up.id}</span>
-            </div>
-            <div className='flex flex-col gap-4 2sm:flex-row'>
-              <span className='font-semibold text-gray-700 dark:text-gray-200'>Model:</span>
-              <RenderBikeModel bikeModel={up.bike!.model} />
-            </div>
-          </div>
-        ))}
-      </div>
+      <div className='flex gap-4'>{usedParkingPlaces?.map((up) => <RenderParkingPlaceTickets key={up.id} parkingPlace={up} />)}</div>
     </div>
   )
 }

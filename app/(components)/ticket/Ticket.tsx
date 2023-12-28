3e5a60f20e4582c160319537da0a542b51325471
h1pasic <100873836+h1pasic@users.noreply.github.com>
2023-12-28 15:44:28 +0100

    Add other elements to the ticket
    bike type
    -bike type
    -Date
    -Time
    -Price
    -Place

--- app/(components)/ticket/Ticket.tsx
+++ app/(components)/ticket/Ticket.tsx
@@ -7,7 +7,63 @@ export default function Ticket() {
         <main className="w-screen h-screen flex flex-col">
             <section className="w-full flex-grow bg-blue flex items-center justify-center p-4">
 
-                
+                <div className="flex w-full max-w-3xl text-zinc-900 h-64">
+                    {/* Linker Teil der Rückseite */}
+                    <div className="h-full py-8 px-10 bg-white flex-grow rounded-l-3xl flex flex-col">
+                        {/* Alle anderen Elemente und Inhalte der Rückseite */}
+                        {/* Weitere Elemente gekürzt für Übersichtlichkeit */}
+
+                        <div className="flex items-center justify-between">
+                            <div>
+                                <h3 className="text-lg font-semibold">Startstation</h3>
+                            </div>
+                            {/* Pfeilsymbol */}
+                            <div>
+                                <svg className="h-6 w-6 text-zinc-900 mx-2" fill="none" strokeLinecap="round"
+                                     strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path d="M4 12h16m0 0l-4-4m4 4l-4 4"></path>
+                                </svg>
+                            </div>
+                            <div>
+                                <h3 className="text-lg font-semibold">Endstation</h3>
+                            </div>
+                        </div>
+
+                        {/* Fahrradart */}
+                        <div className="items-center justify-between mt-8">
+                            <h3 className="text-lg font-semibold">Fahrradart</h3>
+                        </div>
+                        {/* Fahrradtyp */}
+                        <div>
+                            <h3 className="text-lg font-semibold">Fahrradtyp</h3>
+                        </div>
+                        {/* Datum, Zeit, Preis und Platz in derselben Reihe */}
+                        <div className="flex items-center justify-between mt-8">
+                            {/* Datum */}
+                            <div>
+                                <h3 className="text-lg font-semibold">Datum</h3>
+                                <p className="text-md">[Datum]</p>
+                            </div>
+
+                            {/* Zeit */}
+                            <div>
+                                <h3 className="text-lg font-semibold">Zeit</h3>
+                                <p className="text-md">[Zeit]</p>
+                            </div>
+
+                            {/* Preis */}
+                            <div>
+                                <h3 className="text-lg font-semibold">Preis</h3>
+                                <p className="text-md">[Preis]</p>
+                            </div>
+
+                            {/* Platz */}
+                            <div>
+                                <h3 className="text-lg font-semibold">Platz</h3>
+                                <p className="text-md">[Platz]</p>
+                            </div>
+                        </div>
+                    </div>
 
                     {/* Trennlinie */}
                     <div

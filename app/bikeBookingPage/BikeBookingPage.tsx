adc51394658764667d4852c988aefddb5f1dde93
h1pasic <100873836+h1pasic@users.noreply.github.com>
2024-01-04 12:51:25 +0100

    create BikeBookingPage

---
+++ app/bikeBookingPage/BikeBookingPage.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import React, {useContext, useState} from 'react';
+import {TicketsContext} from "@/app/(components)/ticketContext/TicketsContex"; // Der Importpfad kann je nach Projektstruktur variieren
+
+// Importieren Sie zusätzliche Komponenten und Hilfsfunktionen nach Bedarf
+export default function BikeBookingPage() {
+   
+    );
+}

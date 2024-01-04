b9f025fcdcdd369363a885acb1105a73322d709c
h1pasic <100873836+h1pasic@users.noreply.github.com>
2024-01-04 12:52:43 +0100

    Create necessary variables

--- app/bikeBookingPage/BikeBookingPage.tsx
+++ app/bikeBookingPage/BikeBookingPage.tsx
@@ -5,6 +5,18 @@ import {TicketsContext} from "@/app/(components)/ticketContext/TicketsContex"; /
 
 // Importieren Sie zusätzliche Komponenten und Hilfsfunktionen nach Bedarf
 export default function BikeBookingPage() {
+    const [model, setModel] = useState('');
+    const [category, setCategory] = useState('City-Bike');
+    const [station, setStation] = useState('');
+    const [purpose, setPurpose] = useState('');
+    const [rentingTime, setRentingTime] = useState('');
+    const [size, setSize] = useState('Standard');
+    const [rentingOption, setRentingOption] = useState('immediate');
+    const [adultTicket, setAdultTicket] = useState(false); // Zustand für Erwachsenenticket
+    const [numberOfChildren, setNumberOfChildren] = useState(0);
+    const [childrenDetails, setChildrenDetails] = useState([]);
+
+    const {addTicket} = useContext(TicketsContext);
+
    
-    );
 }

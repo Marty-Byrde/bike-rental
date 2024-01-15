c76641d5ca9021a5c9ec2f0f8a1afc9ff5b0d069
h1pasic <100873836+h1pasic@users.noreply.github.com>
2024-01-15 20:28:27 +0100

    Adjust TicketDAO

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 import BikeBookingPage from "@/app/bikeBookingPage/BikeBookingPage";
-import {TicketsProvider} from "@/app/(components)/ticketContext/TicketsContex";
 import TicketsDashboard from "@/app/(components)/ticketsDashboard/TicketsDashboard";
 
 export default function Home() {
     return (
-        <TicketsProvider> {/* TicketProvider umschließt die Komponenten, die Zugriff auf den Ticket-Kontext benötigen */}
-            <div>
-                <h1>This is some content...</h1>
-                <BikeBookingPage/>
-                <TicketsDashboard/>
+        // <TicketsProvider> {/* TicketProvider umschließt die Komponenten, die Zugriff auf den Ticket-Kontext benötigen */}
+        <div>
+            <h1>This is some content...</h1>
+            <BikeBookingPage/>
+            <TicketsDashboard/>
 
-                {/* Weitere Komponenten, die den Ticket-Kontext nutzen */}
-            </div>
-        </TicketsProvider>
+            {/* Weitere Komponenten, die den Ticket-Kontext nutzen */}
+        </div>
+        // </TicketsProvider>
     )
 }

48c296ea6d9b2e7add7b42fbb8b16d5f7ce3acd8
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-01-28 13:51:42 +0100

    refactor: optimized code-base of DisplayBookingOption
    Removed simplistic display of a bike-model and inserted the new component externalized this code-block.

--- app/(components)/stations/book/DisplayBookingOption.tsx
+++ app/(components)/stations/book/DisplayBookingOption.tsx
@@ -1,22 +1,11 @@
-import { BikeModel, ParkingPlace } from "@/typings/Bike"
-import { WithId } from "mongodb"
+import { ParkingPlace } from "@/typings/Bike"
+import RenderSimpleBike from "../../tickets/RenderSimpleBike"
 import { useAvailableStationBikesContext } from "./AvailableStationBikes"
 
 export default function DisplayBookingOption({ parkingPlace }: { parkingPlace: ParkingPlace }) {
   const { id, bike } = parkingPlace
   const { setSelection, isSelected } = useAvailableStationBikesContext()
 
-  const RenderProperty = ({ propertyKey }: { propertyKey: keyof WithId<BikeModel> }) => {
-    if (propertyKey === 'reviews') return null
-    if (propertyKey === '_id') return null
-
-    return (
-      <div className='flex flex-col gap-2 2sm:flex-row 2sm:items-center text-sm'>
-        <span className=''>{propertyKey}:</span>
-        <span className='italic'>{bike!.model[propertyKey]}</span>
-      </div>
-    )
-  }
 
   const handleSelection = () => {
     if (isSelected(parkingPlace)) {
@@ -29,10 +18,7 @@ export default function DisplayBookingOption({ parkingPlace }: { parkingPlace: P
   return (
     <div className='relative flex flex-col gap-2 px-4 py-2 dark:bg-neutral-700/30 ring-1 dark:ring-neutral-700 rounded-md'>
       <span className='absolute -top-0 -right-0 rounded-md dark:bg-yellow-700/70 text-xs px-3 py-1 tracking-wider'>P{id}</span>
-      <div className='flex flex-col gap-2 mt-2'>
-        <h2 className='text-lg font-semibold'>Bike Informations</h2>
-        {Object.keys(bike!.model).map((key) => <RenderProperty key={key} propertyKey={key as keyof WithId<BikeModel>} />)}
-      </div>
+      <RenderSimpleBike bike={bike!} heading="Bike Informations" ignoredModelProps={['_id', 'reviews']} />
 
       <button onClick={handleSelection} hidden={isSelected(parkingPlace)} className='mt-2 px-2 rounded-md text-md py-1 dark:bg-blue-600/70'>
         Add to Cart

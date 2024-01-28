f2d71e622cf8a45829e085ab1b06ef32d98c6e50
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-01-28 13:54:59 +0100

    refactor: removed legacy ticket-implementation from /

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import BikeBookingPage from "@/app/bikeBookingPage/BikeBookingPage";
-import TicketsDashboard from "@/app/(components)/ticketsDashboard/TicketsDashboard";
-
 export default function Home() {
-    return (
-        // <TicketsProvider> {/* TicketProvider umschließt die Komponenten, die Zugriff auf den Ticket-Kontext benötigen */}
-        <div>
-            <h1>This is some content...</h1>
-            <BikeBookingPage/>
-            <TicketsDashboard/>
-
-            {/* Weitere Komponenten, die den Ticket-Kontext nutzen */}
-        </div>
-        // </TicketsProvider>
-    )
+  return (
+    <div>
+      <h1>This is some content...</h1>
+    </div>
+  )
 }

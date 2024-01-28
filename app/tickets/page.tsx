290fa0d70f663ad4614d018b3f5a6e71fe3c1545
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-01-28 14:10:01 +0100

    refactor: changed My Tickets to Tickets - /tickets

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -6,7 +6,7 @@ export default async function TicketsPage() {
 
   return (
     <div>
-      <h1 className='mb-8 text-center text-3xl font-semibold lg:text-left'>My Tickets</h1>
+      <h1 className='mb-8 text-center text-3xl font-semibold lg:text-left'>Tickets</h1>
       <div>
         {tickets.map((ticket) => (
           <RenderTicket ticket={ticket} key={ticket._id.toString()} />
